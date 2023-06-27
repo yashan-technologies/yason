9ca0cc0738000580c34b7d45c4962897877d4288
余睿杰 <yuruijie@sics.ac.cn>
2023-06-27 11:31:20 +0800

    Update sqldatetime dependency

--- tests/format.rs
+++ tests/format.rs
@@ -35,142 +35,142 @@ fn assert_extended_scalar_fmt(input: &str, expected: &str, pretty_expected: &str
 }
 
 #[test]
-fn test_scalar_fmt() {
-    // bool && null
-    {
-        assert_scalar_fmt("true", "true");
-        assert_scalar_fmt("false", "false");
-        assert_scalar_fmt("null", "null");
-    }
+fn test_scalar_bool_and_null_fmt() {
+    assert_scalar_fmt("true", "true");
+    assert_scalar_fmt("false", "false");
+    assert_scalar_fmt("null", "null");
+}
 
-    // string
-    {
-        assert_scalar_fmt(r#""""#, r#""""#);
-        assert_scalar_fmt(r#""abc""#, r#""abc""#);
-        assert_scalar_fmt(r#""测试""#, r#""测试""#);
-        assert_scalar_fmt(r#""ab\bc""#, r#""ab\bc""#);
-        assert_scalar_fmt(r#""ab\fc""#, r#""ab\fc""#);
-        assert_scalar_fmt(r#""ab\nc""#, r#""ab\nc""#);
-        assert_scalar_fmt(r#""ab\rc""#, r#""ab\rc""#);
-        assert_scalar_fmt(r#""ab\tc""#, r#""ab\tc""#);
-        assert_scalar_fmt(r#""ab\"c""#, r#""ab\"c""#);
-        assert_scalar_fmt(r#""ab\\c""#, r#""ab\\c""#);
-        assert_scalar_fmt(r#""ab\/tc""#, r#""ab/tc""#);
+#[test]
+fn test_scalar_string_fmt() {
+    assert_scalar_fmt(r#""""#, r#""""#);
+    assert_scalar_fmt(r#""abc""#, r#""abc""#);
+    assert_scalar_fmt(r#""测试""#, r#""测试""#);
+    assert_scalar_fmt(r#""ab\bc""#, r#""ab\bc""#);
+    assert_scalar_fmt(r#""ab\fc""#, r#""ab\fc""#);
+    assert_scalar_fmt(r#""ab\nc""#, r#""ab\nc""#);
+    assert_scalar_fmt(r#""ab\rc""#, r#""ab\rc""#);
+    assert_scalar_fmt(r#""ab\tc""#, r#""ab\tc""#);
+    assert_scalar_fmt(r#""ab\"c""#, r#""ab\"c""#);
+    assert_scalar_fmt(r#""ab\\c""#, r#""ab\\c""#);
+    assert_scalar_fmt(r#""ab\/tc""#, r#""ab/tc""#);
 
-        assert_scalar_fmt(r#""\u0000""#, r#""\u0000""#);
-        assert_scalar_fmt(r#""\u0001""#, r#""\u0001""#);
-        assert_scalar_fmt(r#""\u0002""#, r#""\u0002""#);
-        assert_scalar_fmt(r#""\u0003""#, r#""\u0003""#);
-        assert_scalar_fmt(r#""\u0004""#, r#""\u0004""#);
-        assert_scalar_fmt(r#""\u0005""#, r#""\u0005""#);
-        assert_scalar_fmt(r#""\u0006""#, r#""\u0006""#);
-        assert_scalar_fmt(r#""\u0007""#, r#""\u0007""#);
-        assert_scalar_fmt(r#""\u0008""#, r#""\b""#);
-        assert_scalar_fmt(r#""\u0009""#, r#""\t""#);
-        assert_scalar_fmt(r#""\u000A""#, r#""\n""#);
-        assert_scalar_fmt(r#""\u000B""#, r#""\u000B""#);
-        assert_scalar_fmt(r#""\u000C""#, r#""\f""#);
-        assert_scalar_fmt(r#""\u000D""#, r#""\r""#);
-        assert_scalar_fmt(r#""\u000E""#, r#""\u000E""#);
-        assert_scalar_fmt(r#""\u000F""#, r#""\u000F""#);
-        assert_scalar_fmt(r#""\u0010""#, r#""\u0010""#);
-        assert_scalar_fmt(r#""\u0011""#, r#""\u0011""#);
-        assert_scalar_fmt(r#""\u0012""#, r#""\u0012""#);
-        assert_scalar_fmt(r#""\u0013""#, r#""\u0013""#);
-        assert_scalar_fmt(r#""\u0014""#, r#""\u0014""#);
-        assert_scalar_fmt(r#""\u0015""#, r#""\u0015""#);
-        assert_scalar_fmt(r#""\u0016""#, r#""\u0016""#);
-        assert_scalar_fmt(r#""\u0017""#, r#""\u0017""#);
-        assert_scalar_fmt(r#""\u0018""#, r#""\u0018""#);
-        assert_scalar_fmt(r#""\u0019""#, r#""\u0019""#);
-        assert_scalar_fmt(r#""\u001A""#, r#""\u001A""#);
-        assert_scalar_fmt(r#""\u001B""#, r#""\u001B""#);
-        assert_scalar_fmt(r#""\u001C""#, r#""\u001C""#);
-        assert_scalar_fmt(r#""\u001D""#, r#""\u001D""#);
-        assert_scalar_fmt(r#""\u001E""#, r#""\u001E""#);
-        assert_scalar_fmt(r#""\u001F""#, r#""\u001F""#);
+    assert_scalar_fmt(r#""\u0000""#, r#""\u0000""#);
+    assert_scalar_fmt(r#""\u0001""#, r#""\u0001""#);
+    assert_scalar_fmt(r#""\u0002""#, r#""\u0002""#);
+    assert_scalar_fmt(r#""\u0003""#, r#""\u0003""#);
+    assert_scalar_fmt(r#""\u0004""#, r#""\u0004""#);
+    assert_scalar_fmt(r#""\u0005""#, r#""\u0005""#);
+    assert_scalar_fmt(r#""\u0006""#, r#""\u0006""#);
+    assert_scalar_fmt(r#""\u0007""#, r#""\u0007""#);
+    assert_scalar_fmt(r#""\u0008""#, r#""\b""#);
+    assert_scalar_fmt(r#""\u0009""#, r#""\t""#);
+    assert_scalar_fmt(r#""\u000A""#, r#""\n""#);
+    assert_scalar_fmt(r#""\u000B""#, r#""\u000B""#);
+    assert_scalar_fmt(r#""\u000C""#, r#""\f""#);
+    assert_scalar_fmt(r#""\u000D""#, r#""\r""#);
+    assert_scalar_fmt(r#""\u000E""#, r#""\u000E""#);
+    assert_scalar_fmt(r#""\u000F""#, r#""\u000F""#);
+    assert_scalar_fmt(r#""\u0010""#, r#""\u0010""#);
+    assert_scalar_fmt(r#""\u0011""#, r#""\u0011""#);
+    assert_scalar_fmt(r#""\u0012""#, r#""\u0012""#);
+    assert_scalar_fmt(r#""\u0013""#, r#""\u0013""#);
+    assert_scalar_fmt(r#""\u0014""#, r#""\u0014""#);
+    assert_scalar_fmt(r#""\u0015""#, r#""\u0015""#);
+    assert_scalar_fmt(r#""\u0016""#, r#""\u0016""#);
+    assert_scalar_fmt(r#""\u0017""#, r#""\u0017""#);
+    assert_scalar_fmt(r#""\u0018""#, r#""\u0018""#);
+    assert_scalar_fmt(r#""\u0019""#, r#""\u0019""#);
+    assert_scalar_fmt(r#""\u001A""#, r#""\u001A""#);
+    assert_scalar_fmt(r#""\u001B""#, r#""\u001B""#);
+    assert_scalar_fmt(r#""\u001C""#, r#""\u001C""#);
+    assert_scalar_fmt(r#""\u001D""#, r#""\u001D""#);
+    assert_scalar_fmt(r#""\u001E""#, r#""\u001E""#);
+    assert_scalar_fmt(r#""\u001F""#, r#""\u001F""#);
 
-        assert_scalar_fmt(r#""\u0022""#, r#""\"""#);
-        assert_scalar_fmt(r#""\u002F""#, r#""/""#);
-        assert_scalar_fmt(r#""\u005c""#, r#""\\""#);
-        assert_scalar_fmt(r#""\u007F""#, r#""\u007F""#);
-        assert_scalar_fmt(r#""\u007f""#, r#""\u007F""#);
-    }
+    assert_scalar_fmt(r#""\u0022""#, r#""\"""#);
+    assert_scalar_fmt(r#""\u002F""#, r#""/""#);
+    assert_scalar_fmt(r#""\u005c""#, r#""\\""#);
+    assert_scalar_fmt(r#""\u007F""#, r#""\u007F""#);
+    assert_scalar_fmt(r#""\u007f""#, r#""\u007F""#);
+}
 
-    // number
-    {
-        assert_scalar_fmt("123", "123");
-        assert_scalar_fmt("12340", "12340");
-        assert_scalar_fmt("123.123", "123.123");
-        assert_scalar_fmt("-123", "-123");
-        assert_scalar_fmt("-12300000", "-12300000");
-        assert_scalar_fmt("1234567890.123456789", "1234567890.123456789");
-        assert_scalar_fmt("12300e35", "1230000000000000000000000000000000000000");
-        assert_scalar_fmt("12300e36", "1.23E+40");
-        assert_scalar_fmt("123e37", "1230000000000000000000000000000000000000");
-        assert_scalar_fmt("123e38", "1.23E+40");
-        assert_scalar_fmt("-12300e35", "-1230000000000000000000000000000000000000");
-        assert_scalar_fmt("-12300e36", "-1.23E+40");
-        assert_scalar_fmt("-123e37", "-1230000000000000000000000000000000000000");
-        assert_scalar_fmt("-123e38", "-1.23E+40");
-        assert_scalar_fmt("123e-41", "1.23E-39");
-        assert_scalar_fmt("123e-40", "0.0000000000000000000000000000000000000123");
-        assert_scalar_fmt("12300e-43", "1.23E-39");
-        assert_scalar_fmt("12300e-42", "0.0000000000000000000000000000000000000123");
-        assert_scalar_fmt("-123e-41", "-1.23E-39");
-        assert_scalar_fmt("-123e-40", "-0.0000000000000000000000000000000000000123");
-        assert_scalar_fmt("-12300e-43", "-1.23E-39");
-        assert_scalar_fmt("-12300e-42", "-0.0000000000000000000000000000000000000123");
-        assert_scalar_fmt(
-            "1234567890123456789012345678901234567800e-42",
-            "0.0012345678901234567890123456789012345678",
-        );
-        assert_scalar_fmt(
-            "1234567890123456789012345678901234567800e-43",
-            "1.2345678901234567890123456789012345678E-4",
-        );
-        assert_scalar_fmt(
-            "12345678901234567.890123456789012345678e23",
-            "1234567890123456789012345678901234567800",
-        );
-        assert_scalar_fmt(
-            "12345678901234567.890123456789012345678e24",
-            "1.2345678901234567890123456789012345678E+40",
-        );
-        assert_scalar_fmt(
-            "12345678901234567.890123456789012345678e-19",
-            "0.0012345678901234567890123456789012345678",
-        );
-        assert_scalar_fmt(
-            "12345678901234567.890123456789012345678e-21",
-            "1.2345678901234567890123456789012345678E-5",
-        );
-        assert_scalar_fmt(
-            "0.00000000012345678901234567890123456789012345678e-1",
-            "1.2345678901234567890123456789012345678E-11",
-        );
-        assert_scalar_fmt(
-            "0.00000000012345678901234567890123456789012345678e6",
-            "1.2345678901234567890123456789012345678E-4",
-        );
-        assert_scalar_fmt(
-            "0.00000000012345678901234567890123456789012345678e7",
-            "0.0012345678901234567890123456789012345678",
-        );
-        assert_scalar_fmt(
-            "0.00000000012345678901234567890123456789012345678e47",
-            "12345678901234567890123456789012345678",
-        );
-        assert_scalar_fmt(
-            "0.00000000012345678901234567890123456789012345678e49",
-            "1234567890123456789012345678901234567800",
-        );
-        assert_scalar_fmt(
-            "0.00000000012345678901234567890123456789012345678e50",
-            "1.2345678901234567890123456789012345678E+40",
-        );
-    }
+#[test]
+fn test_scalar_number_fmt() {
+    assert_scalar_fmt("123", "123");
+    assert_scalar_fmt("12340", "12340");
+    assert_scalar_fmt("123.123", "123.123");
+    assert_scalar_fmt("-123", "-123");
+    assert_scalar_fmt("-12300000", "-12300000");
+    assert_scalar_fmt("1234567890.123456789", "1234567890.123456789");
+    assert_scalar_fmt("12300e35", "1230000000000000000000000000000000000000");
+    assert_scalar_fmt("12300e36", "1.23E+40");
+    assert_scalar_fmt("123e37", "1230000000000000000000000000000000000000");
+    assert_scalar_fmt("123e38", "1.23E+40");
+    assert_scalar_fmt("-12300e35", "-1230000000000000000000000000000000000000");
+    assert_scalar_fmt("-12300e36", "-1.23E+40");
+    assert_scalar_fmt("-123e37", "-1230000000000000000000000000000000000000");
+    assert_scalar_fmt("-123e38", "-1.23E+40");
+    assert_scalar_fmt("123e-41", "1.23E-39");
+    assert_scalar_fmt("123e-40", "0.0000000000000000000000000000000000000123");
+    assert_scalar_fmt("12300e-43", "1.23E-39");
+    assert_scalar_fmt("12300e-42", "0.0000000000000000000000000000000000000123");
+    assert_scalar_fmt("-123e-41", "-1.23E-39");
+    assert_scalar_fmt("-123e-40", "-0.0000000000000000000000000000000000000123");
+    assert_scalar_fmt("-12300e-43", "-1.23E-39");
+    assert_scalar_fmt("-12300e-42", "-0.0000000000000000000000000000000000000123");
+    assert_scalar_fmt(
+        "1234567890123456789012345678901234567800e-42",
+        "0.0012345678901234567890123456789012345678",
+    );
+    assert_scalar_fmt(
+        "1234567890123456789012345678901234567800e-43",
+        "1.2345678901234567890123456789012345678E-4",
+    );
+    assert_scalar_fmt(
+        "12345678901234567.890123456789012345678e23",
+        "1234567890123456789012345678901234567800",
+    );
+    assert_scalar_fmt(
+        "12345678901234567.890123456789012345678e24",
+        "1.2345678901234567890123456789012345678E+40",
+    );
+    assert_scalar_fmt(
+        "12345678901234567.890123456789012345678e-19",
+        "0.0012345678901234567890123456789012345678",
+    );
+    assert_scalar_fmt(
+        "12345678901234567.890123456789012345678e-21",
+        "1.2345678901234567890123456789012345678E-5",
+    );
+    assert_scalar_fmt(
+        "0.00000000012345678901234567890123456789012345678e-1",
+        "1.2345678901234567890123456789012345678E-11",
+    );
+    assert_scalar_fmt(
+        "0.00000000012345678901234567890123456789012345678e6",
+        "1.2345678901234567890123456789012345678E-4",
+    );
+    assert_scalar_fmt(
+        "0.00000000012345678901234567890123456789012345678e7",
+        "0.0012345678901234567890123456789012345678",
+    );
+    assert_scalar_fmt(
+        "0.00000000012345678901234567890123456789012345678e47",
+        "12345678901234567890123456789012345678",
+    );
+    assert_scalar_fmt(
+        "0.00000000012345678901234567890123456789012345678e49",
+        "1234567890123456789012345678901234567800",
+    );
+    assert_scalar_fmt(
+        "0.00000000012345678901234567890123456789012345678e50",
+        "1.2345678901234567890123456789012345678E+40",
+    );
+}
 
+#[test]
+fn test_scalar_ext_numeric_fmt() {
     assert_extended_scalar_fmt(r#"{"$numberByte": 127}"#, r#"127"#, r#"127"#);
     assert_extended_scalar_fmt(r#"{"$numberShort": 12345}"#, r#"12345"#, r#"12345"#);
     assert_extended_scalar_fmt(r#"{"$numberInt": 1234567}"#, r#"1234567"#, r#"1234567"#);
@@ -206,7 +206,10 @@ fn test_scalar_fmt() {
         r#"{"$numberLong":"9007199254740992"}"#,
         "{\n  \"$numberLong\" : \"9007199254740992\"\n}",
     );
+}
 
+#[test]
+fn test_scalar_binary_fmt() {
     assert_extended_scalar_fmt(r#"{"$binary": ""}"#, r#"{"$binary":""}"#, "{\n  \"$binary\" : \"\"\n}");
     assert_extended_scalar_fmt(
         r#"{"$binary": "abc"}"#,
@@ -225,7 +228,10 @@ fn test_scalar_fmt() {
         true,
         false,
     );
+}
 
+#[test]
+fn test_scalar_timestamp_fmt() {
     assert_extended_scalar_fmt(
         r#"{"$yashanTimestamp": "2023-05-25T16:50:20.123"}"#,
         r#"{"$yashanTimestamp":"2023-05-25T16:50:20.123000"}"#,
@@ -251,7 +257,44 @@ fn test_scalar_fmt() {
         r#"{"$yashanTimestamp":"2023-05-25T16:50:20.123000"}"#,
         "{\n  \"$yashanTimestamp\" : \"2023-05-25T16:50:20.123000\"\n}",
     );
+    assert_extended_scalar_fmt(
+        r#"{"$oracleTimestamp": "9999-12-31T23:59:59.999999"}"#,
+        r#"{"$yashanTimestamp":"9999-12-31T23:59:59.999999"}"#,
+        "{\n  \"$yashanTimestamp\" : \"9999-12-31T23:59:59.999999\"\n}",
+    );
+    assert_extended_scalar_fmt(
+        r#"{"$oracleTimestamp": "9999-12-31T23:59:59.999999999"}"#,
+        // fail to parse as extended because overflow
+        r#"{"$oracleTimestamp":"9999-12-31T23:59:59.999999999"}"#,
+        "{\n  \"$oracleTimestamp\" : \"9999-12-31T23:59:59.999999999\"\n}",
+    );
+    assert_extended_scalar_fmt(
+        r#"{"$oracleTimestamp": "9999-12-31T23:59:59.9999999999"}"#,
+        // fail to parse as extended because too long
+        r#"{"$oracleTimestamp":"9999-12-31T23:59:59.9999999999"}"#,
+        "{\n  \"$oracleTimestamp\" : \"9999-12-31T23:59:59.9999999999\"\n}",
+    );
+    assert_extended_scalar_fmt(
+        r#"{"$oracleTimestamp": "9999-12-30T23:59:59.999999"}"#,
+        r#"{"$yashanTimestamp":"9999-12-30T23:59:59.999999"}"#,
+        "{\n  \"$yashanTimestamp\" : \"9999-12-30T23:59:59.999999\"\n}",
+    );
+    assert_extended_scalar_fmt(
+        r#"{"$oracleTimestamp": "9999-12-30T23:59:59.999999999"}"#,
+        // round up
+        r#"{"$yashanTimestamp":"9999-12-31T00:00:00"}"#,
+        "{\n  \"$yashanTimestamp\" : \"9999-12-31T00:00:00\"\n}",
+    );
+    assert_extended_scalar_fmt(
+        r#"{"$oracleTimestamp": "9999-12-30T23:59:59.9999999999"}"#,
+        // fail to parse as extended because too long
+        r#"{"$oracleTimestamp":"9999-12-30T23:59:59.9999999999"}"#,
+        "{\n  \"$oracleTimestamp\" : \"9999-12-30T23:59:59.9999999999\"\n}",
+    );
+}
 
+#[test]
+fn test_scalar_date_fmt() {
     assert_extended_scalar_fmt(
         r#"{"$yashanDate": "2023-05-25T16:50:20"}"#,
         r#"{"$yashanDate":"2023-05-25T16:50:20"}"#,
@@ -277,7 +320,27 @@ fn test_scalar_fmt() {
         r#"{"$yashanDate":"2023-05-25T16:50:20"}"#,
         "{\n  \"$yashanDate\" : \"2023-05-25T16:50:20\"\n}",
     );
+    assert_extended_scalar_fmt(
+        r#"{"$oracleDate": "9999-12-31T23:59:59.999999"}"#,
+        r#"{"$yashanDate":"9999-12-31T23:59:59"}"#,
+        "{\n  \"$yashanDate\" : \"9999-12-31T23:59:59\"\n}",
+    );
+    assert_extended_scalar_fmt(
+        r#"{"$oracleDate": "9999-12-31T23:59:59.999999999"}"#,
+        // When below 9 digits, truncate
+        r#"{"$yashanDate":"9999-12-31T23:59:59"}"#,
+        "{\n  \"$yashanDate\" : \"9999-12-31T23:59:59\"\n}",
+    );
+    assert_extended_scalar_fmt(
+        r#"{"$oracleDate": "9999-12-31T23:59:59.9999999999"}"#,
+        // fail to parse as extended because too long
+        r#"{"$oracleDate":"9999-12-31T23:59:59.9999999999"}"#,
+        "{\n  \"$oracleDate\" : \"9999-12-31T23:59:59.9999999999\"\n}",
+    );
+}
 
+#[test]
+fn test_scalar_time_fmt() {
     assert_extended_scalar_fmt(
         r#"{"$yashanTime": "16:50:20"}"#,
         r#"{"$yashanTime":"16:50:20"}"#,
